5dde027949bd3861241393cd6ff48eb2e8719b69
Ash <asherchez@gmail.com>
2020-10-04 15:32:39 +0100

    Updates

---
+++ precache-manifest.1833a8bee870124cdaec55a5a7c214a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa3395b704a9b38097581ad07fb6ef5",
+    "url": "/react-sketch/index.html"
+  },
+  {
+    "revision": "e118da766179656a5c96",
+    "url": "/react-sketch/static/css/main.60a543f3.chunk.css"
+  },
+  {
+    "revision": "e3a30063306715f96f8c",
+    "url": "/react-sketch/static/js/2.4610b90b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-sketch/static/js/2.4610b90b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e118da766179656a5c96",
+    "url": "/react-sketch/static/js/main.bb1ccefa.chunk.js"
+  },
+  {
+    "revision": "a8efb1c8d432d5c5ef53",
+    "url": "/react-sketch/static/js/runtime-main.0a53fb08.js"
+  }
+]);
